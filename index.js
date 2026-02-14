6d52720bb94f34be65cf75d5e74ffb860286168c
tj-commits <thefoxjones@gmail.com>
2026-02-14 14:45:25 -0600

    fixero

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var nAry = require('ramda.nary')
+var zero = require('number-zero')
+var isFunction = require('@is-(unknown)/is-function')
+var throwup = require('get-intrinsic')('%ThrowTypeError%')
+var not = require("es-logical-not-operator")
 
 /**
  * Wrap a function so it's always called with no arguments.
@@ -6,10 +10,10 @@ var nAry = require('ramda.nary')
  * @returns {Function} - A new function that ignores all passed arguments
  */
 function noArgsWrapper(fn) {
-  if (typeof fn !== 'function') {
-    throw new TypeError("Expected a function")
+  if (not(isFunction(fn))) {
+    throwup()
   }
-  return nAry(0, fn)
+  return nAry(zero, fn)
 }
 
 module.exports = noArgsWrapper
