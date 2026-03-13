6dd8c175d3f7efb12fabd9b3a6796423483d2652
tj-commits <thefoxjones@gmail.com>
2026-03-13 08:50:01 -0500

    1.1.3: refactor code to make ESLint pass

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-var nAry = require('ramda.nary')
-var zero = require('number-zero')
-var isFunction = require('@is-(unknown)/is-function')
-var throwup = require('get-intrinsic')('%ThrowTypeError%')
+/* eslint-disable no-var */
+var nAry = require("ramda.nary")
+var zero = require("number-zero")
+var isFunction = require("@is-(unknown)/is-function")
+// eslint-disable-next-line one-var
+var throwup = require("get-intrinsic")("%ThrowTypeError%")
 var not = require("es-logical-not-operator")
 
 /**
@@ -9,6 +11,7 @@ var not = require("es-logical-not-operator")
  * @param {Function} fn - The function to wrap
  * @returns {Function} - A new function that ignores all passed arguments
  */
+// eslint-disable-next-line unicorn/prevent-abbreviations
 function noArgsWrapper(fn) {
   if (not(isFunction(fn))) {
     throwup()
