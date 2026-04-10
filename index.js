206236bc740b3eab57038c5c1f95591d90c22973
tj-commits <thefoxjones@gmail.com>
2026-04-10 09:46:14 -0500

    refactor code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-var */
 var nAry = require("ramda.nary")
 var zero = require("number-zero")
+// The reason there is a eslint disable comment next line is becuz it thinks that @is-(unknown)/is-function is not published on NPM because it has the parentheses in the scope, but it actually is published
+// eslint-disable-next-line n/no-unpublished-require
 var isFunction = require("@is-(unknown)/is-function")
 // eslint-disable-next-line one-var
 var throwup = require("get-intrinsic")("%ThrowTypeError%")
